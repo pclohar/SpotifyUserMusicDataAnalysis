33318455308c621ffc3d67371366065cf926706b
Pallavi Lohar <pallavi.lohar18@gmail.com>
2020-06-30 15:40:08 -0400

    Removed setupProxy.js

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app) {
-    app.use(createProxyMiddleware('/auth/user', { target : 'https://spotify-music-analytics-server.herokuapp.com' }));
-    app.use(createProxyMiddleware('/auth/redirect-spotify', { target : 'https://spotify-music-analytics-server.herokuapp.com' }));
-    app.use(createProxyMiddleware('/songs/recent', { target : 'https://spotify-music-analytics-server.herokuapp.com' }));
-    app.use(createProxyMiddleware('/songs/top', { target : 'https://spotify-music-analytics-server.herokuapp.com' }));
-}
