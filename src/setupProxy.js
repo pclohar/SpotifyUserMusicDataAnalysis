c9e21cb905c48442ad5611d79208bce4cd0e1aac
Pallavi Lohar <pallavi.lohar18@gmail.com>
2020-07-02 00:08:35 -0400

    added build

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/auth/user', { target : 'http://localhost:5000' }));
-    app.use(createProxyMiddleware('/auth/redirect-spotify', { target : 'http://localhost:5000' }));
-    app.use(createProxyMiddleware('/songs/recent', { target : 'http://localhost:5000' }));
-    app.use(createProxyMiddleware('/songs/top', { target : 'http://localhost:5000' }));
+    app.use(createProxyMiddleware('/auth/user', { target : 'https://spotify-music-analytics-server.herokuapp.com' }));
+    app.use(createProxyMiddleware('/auth/redirect-spotify', { target : 'https://spotify-music-analytics-server.herokuapp.com' }));
+    app.use(createProxyMiddleware('/songs/recent', { target : 'https://spotify-music-analytics-server.herokuapp.com' }));
+    app.use(createProxyMiddleware('/songs/top', { target : 'https://spotify-music-analytics-server.herokuapp.com' }));
 }
