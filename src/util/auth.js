b034dcd2f1deec56cf654956b1a9fc9d8322fd35
Pallavi Lohar <pallavi.lohar18@gmail.com>
2020-07-05 13:09:25 -0400

    Testing frontend

--- src/util/auth.js
+++ src/util/auth.js
@@ -11,7 +11,7 @@ const config = {
 export function getLoginRedirect() {
   return new Promise((resolve, reject) => {
     axios
-      .get('https://spotify-music-analytics-server.herokuapp.com/auth/redirect-spotify')
+      .get('http://localhost:5000/auth/redirect-spotify')
       .then(res => resolve(res.data), err => reject(err));
   });
 }
@@ -19,7 +19,7 @@ export function getLoginRedirect() {
 export function registerSpotify(code) {
   return new Promise((resolve, reject) => {
     axios
-      .post('https://spotify-music-analytics-server.herokuapp.com/auth/user', { code: removeHashParams(code) }, config)
+      .post('http://localhost:5000/auth/user', { code: removeHashParams(code) }, config)
       .then(
         res => resolve(res.data),
         err => reject(err)
@@ -30,7 +30,7 @@ export function registerSpotify(code) {
 export function getSpotifyUser() {
   return new Promise((resolve, reject) => {
     axios
-      .get('https://spotify-music-analytics-server.herokuapp.com/auth/user')
+      .get('http://localhost:5000/auth/user')
       .then(
         res => resolve(res.data),
         err => reject(err)
@@ -41,7 +41,7 @@ export function getSpotifyUser() {
 export function logout() {
   return new Promise((resolve, reject) => {
     axios
-      .get('https://spotify-music-analytics-server.herokuapp.com/auth/logout')
+      .get('http://localhost:5000/auth/logout')
       .then(
         res => resolve(res),
         err => reject(err)
