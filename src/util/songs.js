222c4f0c44ab6c7a3fa0e61e1b275bee82b6234b
Pallavi Lohar <pallavi.lohar18@gmail.com>
2020-07-06 13:57:51 -0400

    adding logs to check songs.js

--- src/util/songs.js
+++ src/util/songs.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export function getTopSongs() {
+  console.log('In promise')
   return new Promise((resolve, reject) => {
     axios.get('https://spotify-music-analytics-server.herokuapp.com/songs/top').then(
       res => resolve(res.data),
